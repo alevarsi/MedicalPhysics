d732913dfddafa0d51b60d75d8e3f1fe6d041426
alevarsi <varsialessandro@gmail.com>
2025-11-24 18:40:08 +0100

    update

--- src/attenuation.py
+++ src/attenuation.py
@@ -8,23 +8,31 @@ plt.rcParams.update({
     "font.family": "Helvetica"
 })
 
+exponent = -2.75
+material = "Lead"
+
 def func(x, a, b):
-    return a + b * x ** (-3.5)
+    return a + b * x ** (exponent)
+
+#def func(x, a, b):
+#    return a + b * np.exp(-x)
 
 def analyze_attenuation(data_file, output_image, photon_energy):
     # Load data
-    data = np.loadtxt("../data/"+data_file, 
+    data = np.loadtxt("../data/"+material + "_" + data_file, 
                       #delimiter='\t'
                       )
     energy = data[:, 0]*1000 #energy in keV
     attenuation = data[:, 1] #attenuation/density in cm^2/g
 
-    popt, pcov = curve_fit(func, energy, attenuation, p0=(0, 1))
+    popt, pcov = curve_fit(func, energy, attenuation, 
+                           p0=(0, 10)
+                           )
     a_fit, b_fit = popt
 
     # Print fitted parameters
     coeff = func(photon_energy, a_fit, b_fit)
-    print(f'Attenuation coefficient at {photon_energy} keV: {coeff} cm^2/g')
+    print(f'Mass Attenuation coefficient of Lead at {photon_energy} keV: {coeff} cm^2/g')
 
     # Generate fitted values for plotting
     energy_fit = np.linspace(min(energy), max(energy), 100)
@@ -34,18 +42,19 @@ def analyze_attenuation(data_file, output_image, photon_energy):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(energy, attenuation, label='Measured Data', color='blue')
-    ax.plot(energy_fit, fitted_func, label=r'Fit $\propto E_\gamma^{-7/2}$', color='red')
-    ax.set_title("Attenuation with respect to Photon Energy")
+    ax.plot(energy_fit, fitted_func, label=r'Fit $\propto E_\gamma^{-2.75}$', color='red')
+    ax.set_title("Mass attenuation coefficient for " + material)
     ax.set_xlabel(r"Photon Energy $E_\gamma$ (keV)")
     ax.set_ylabel(r"$\frac{\mu}{\rho}$ (cm$^2$/g)")
     #ax.set_yscale('log')
     ax.legend()
     ax.grid()
     line, = ax.plot(photon_energy, coeff, color='#ee8d18', lw=2)
-    ax.annotate('(120 keV, {:.2f} cm$^2$/g)'.format(coeff),
+    ax.annotate('({photon_energy} keV, {coeff:.2f} cm$^2$/g)'.format(photon_energy=photon_energy, coeff=coeff),
             xy=(photon_energy, coeff),  # positions
             xytext=(0.5, 0.5),    # fraction, fraction
             textcoords='figure fraction',
+            fontsize=14,
             arrowprops=dict(facecolor='black', shrink=0.07, width=0.8, headwidth=5),
             horizontalalignment='left',
             verticalalignment='top',
@@ -56,4 +65,6 @@ def analyze_attenuation(data_file, output_image, photon_energy):
     fig.show()
 
 energy = 120 # keV
-analyze_attenuation("attenuation_data.txt", "attenuation_plot.pdf", energy)
+#energy_peak = 57.98 # keV
+#analyze_attenuation("attenuation_data.txt", "attenuation_plot.png", energy)
+analyze_attenuation("attenuation_data.txt", material + "_attenuation_data_plot.png", energy)
