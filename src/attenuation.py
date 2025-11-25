d1c32afe46e121f15af1dc763485b57bc2e9e6b9
alevarsi <varsialessandro@gmail.com>
2025-11-25 10:27:24 +0100

    up

--- src/attenuation.py
+++ src/attenuation.py
@@ -15,7 +15,7 @@ def func(x, a, b):
     return a + b * x ** (exponent)
 
 #def func(x, a, b):
-#    return a + b * np.exp(-x)
+#    return a + b * x
 
 def analyze_attenuation(data_file, output_image, photon_energy):
     # Load data
@@ -32,7 +32,7 @@ def analyze_attenuation(data_file, output_image, photon_energy):
 
     # Print fitted parameters
     coeff = func(photon_energy, a_fit, b_fit)
-    print(f'Mass Attenuation coefficient of Lead at {photon_energy} keV: {coeff} cm^2/g')
+    print(f"Mass Attenuation coefficient of Lead at {photon_energy} keV: {coeff} cm^2/g")
 
     # Generate fitted values for plotting
     energy_fit = np.linspace(min(energy), max(energy), 100)
@@ -48,7 +48,7 @@ def analyze_attenuation(data_file, output_image, photon_energy):
     ax.set_ylabel(r"$\frac{\mu}{\rho}$ (cm$^2$/g)")
     #ax.set_yscale('log')
     ax.legend()
-    ax.grid()
+    #ax.grid()
     line, = ax.plot(photon_energy, coeff, color='#ee8d18', lw=2)
     ax.annotate('({photon_energy} keV, {coeff:.2f} cm$^2$/g)'.format(photon_energy=photon_energy, coeff=coeff),
             xy=(photon_energy, coeff),  # positions
@@ -65,6 +65,6 @@ def analyze_attenuation(data_file, output_image, photon_energy):
     fig.show()
 
 #energy = 120 # keV
-energy_peak = 59.32 # keV
+energy = 59.32 # keV
 #analyze_attenuation("attenuation_data.txt", "attenuation_plot.png", energy)
-analyze_attenuation("attenuation_peak.txt", material + "_attenuation_peak_plot.pdf", energy_peak)
+analyze_attenuation("attenuation_peak.txt", material + "_attenuation_peak_plot.pdf", energy)
